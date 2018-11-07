0dde1a6cf4c9a325a89e6807d377cacef8204cf8
Vladislav Valevaty <valevaty98@gmail.com>
2018-11-07 20:11:19 +0300

    Added abstract page class

---
+++ Insomnia/src/pages/Page.java
@@ -0,0 +1,25 @@
+package pages;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public abstract class Page {
+    protected void openNewScene(Node node, String window) {
+        node.getScene().getWindow().hide();
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(window));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 700, 500));
+        stage.showAndWait();
+    }
+}
