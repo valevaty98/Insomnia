f29644f798befc69f5be6fad8ee7172715d8999e
Vladislav Valevaty <valevaty98@gmail.com>
2018-11-07 19:03:03 +0300

    Added add and edit scenes

--- Insomnia/src/pages/add/AddController.java
+++ Insomnia/src/pages/add/AddController.java
@@ -1,4 +1,82 @@
 package pages.add;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+
 public class AddController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView booksImage;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private TextField authorField;
+
+    @FXML
+    private RadioButton haveReadRadio;
+
+    @FXML
+    private ToggleGroup typeRadio;
+
+    @FXML
+    private RadioButton isReadingRadio;
+
+    @FXML
+    private RadioButton willReadRadio;
+
+    @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private DatePicker tillDatePicker;
+
+    @FXML
+    private CheckBox isAudioCheck;
+
+    @FXML
+    private ChoiceBox<?> genreComboBox;
+
+    @FXML
+    private TextArea noteField;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Button aboutButton;
+
+    @FXML
+    private Button isReadingButton;
+
+    @FXML
+    private Button willReadButton;
+
+    @FXML
+    private Button haveReadButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    void initialize() {
+
+    }
 }
--- Insomnia/src/pages/edit/EditController.java
+++ Insomnia/src/pages/edit/EditController.java
@@ -1,4 +1,84 @@
 package pages.edit;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class EditController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private TextField authorField;
+
+    @FXML
+    private RadioButton haveReadRadio;
+
+    @FXML
+    private ToggleGroup typeRadio;
+
+    @FXML
+    private RadioButton isReadingRadio;
+
+    @FXML
+    private RadioButton willReadRadio;
+
+    @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private DatePicker tillDatePicker;
+
+    @FXML
+    private CheckBox isAudioCheck;
+
+    @FXML
+    private ChoiceBox<?> genreComboBox;
+
+    @FXML
+    private TextArea noteField;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button aboutButton;
+
+    @FXML
+    private Button isReadingButton;
+
+    @FXML
+    private Button willReadButton;
+
+    @FXML
+    private Button haveReadButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    void initialize() {
+
+    }
 }
