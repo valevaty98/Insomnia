6593092d985606ad3d37f8e6ba67418e36e3a04f
Vladislav Valevaty <valevaty98@gmail.com>
2018-11-07 20:59:42 +0300

    Updated main and have read scenes

--- Insomnia/src/pages/haveread/HaveReadController.java
+++ Insomnia/src/pages/haveread/HaveReadController.java
@@ -4,9 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import pages.Page;
 
-public class HaveReadController {
+public class HaveReadController extends Page {
 
     @FXML
     private ResourceBundle resources;
@@ -15,22 +17,50 @@ public class HaveReadController {
     private URL location;
 
     @FXML
+    private ListView<?> haveReadList;
+
+    @FXML
+    private ImageView addImage;
+
+    @FXML
     private Button isReadingButton;
 
     @FXML
-    private ImageView booksImage;
+    private Button aboutButton;
 
     @FXML
     private Button willReadButton;
 
     @FXML
-    private Button haveReadButton;
-
-    @FXML
     private Button homeButton;
 
     @FXML
+    private Button haveReadButton;
+
+    @FXML
     void initialize() {
-        
+        aboutButton.setOnMouseClicked(event -> {
+            openNewScene(aboutButton, "/pages/about/about.fxml");
+        });
+
+        homeButton.setOnMouseClicked(event -> {
+            openNewScene(homeButton, "/pages/mainpage/mainage.fxml");
+        });
+
+        willReadButton.setOnMouseClicked(event -> {
+            openNewScene(willReadButton, "/pages/willread/willread.fxml");
+        });
+
+        isReadingButton.setOnMouseClicked(event -> {
+            openNewScene(isReadingButton, "/pages/isreading/isreading.fxml");
+        });
+
+        haveReadButton.setOnMouseClicked(event -> {
+            openNewScene(haveReadButton, "/pages/haveread/haveread.fxml");
+        });
+
+        addImage.setOnMouseClicked(event -> {
+            openNewScene(addImage, "/pages/add/add.fxml");
+        });
     }
 }
